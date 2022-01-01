af40edaad90e833e27a535e694bc4dce426558c9
Dream Garden (rey) <93763859+HELZRIP@users.noreply.github.com>
2022-01-01 21:14:05 +0700

    Update alive.py

--- lunaBot/modules/alive.py
+++ lunaBot/modules/alive.py
@@ -12,13 +12,13 @@ PHOTO = "https://telegra.ph/file/8c29652140f01b3449895.jpg"
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  LUNA = "**Holla I'm R robot!** \n\n"
+  LUNA = "**Holla I'm Akeno!** \n\n"
   LUNA += "🔴 **I'm Working Properly** \n\n"
   LUNA += "🔴 **My Master : [Dream Garden (rey)](https://t.me/helzrip)** \n\n"
   LUNA += f"🔴 **Telethon Version : {tlhver}** \n\n"
   LUNA += f"🔴 **Pyrogram Version : {pyrover}** \n\n"
   LUNA += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/R_reyrobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/Rsupportprobot")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Akeno?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/Rsupportprobot")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
 
 @MEMEK(pattern=("/reload"))
